a7df966dc945f8807aa3c94efe931492f3e1799a
CristinaaFlorea <floreacristina30@yahoo.ro>
2022-12-28 03:10:39 +0200

    Add project files.

---
+++ Proiect/Data/ShoppingListDatabase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Proiect.Models;
+
+
+namespace Proiect.Data
+{
+    public class ShoppingListDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+        public ShoppingListDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<ShoppingList>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
+        }
+        public Task<List<ShoppingList>> GetShopListsAsync()
+        {
+            return _database.Table<ShoppingList>().ToListAsync();
+        }
+        public Task<ShoppingList> GetShopListAsync(int id)
+        {
+            return _database.Table<ShoppingList>()
+            .Where(i => i.ID == id)
+           .FirstOrDefaultAsync();
+        }
+        public Task<int> SaveShopListAsync(ShoppingList slist)
+        {
+            if (slist.ID != 0)
+            {
+                return _database.UpdateAsync(slist);
+            }
+            else
+            {
+                return _database.InsertAsync(slist);
+            }
+        }
+        public Task<int> DeleteShopListAsync(ShoppingList slist)
+        {
+            return _database.DeleteAsync(slist);
+        }
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
+        }
+    }
+}
---
+++ Proiect/ListPage.xaml.cs
@@ -0,0 +1,37 @@
+using Proiect.Models;
+
+namespace Proiect;
+
+public partial class ListPage : ContentPage
+{
+    public ListPage()
+    {
+        InitializeComponent();
+    }
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShoppingList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        Shop selectedShop = (ShopPicker.SelectedItem as Shop);
+        slist.ShopID = selectedShop.ID;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShoppingList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var items = await App.Database.GetShopsAsync();
+        ShopPicker.ItemsSource = (System.Collections.IList)items;
+        ShopPicker.ItemDisplayBinding = new Binding("ShopDetails");
+
+        var shopl = (ShoppingList)BindingContext;
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+}
